0c7137f2ef41f8c2633d7da47fd63aa2f26bef93
DamZav <damaris.zavala91@gmail.com>
2024-09-13 15:51:27 -0600

    Controller y edición del Service de user

--- alephart/src/main/java/com/stella/alephart/controllers/UserController.java
+++ alephart/src/main/java/com/stella/alephart/controllers/UserController.java
@@ -1,5 +1,56 @@
 package com.stella.alephart.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stella.alephart.models.User;
+import com.stella.alephart.services.UserService;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getId_user(@PathVariable("id") Long id) {
+		return userService.findUserById(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		return userService.saveUser(user);
+	}
+
+	//@PutMapping("/id")
+	public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User updatedUser) {
+        return userService.findUserById(id)
+            .map(user -> {
+                user.setPassword(updatedUser.getPassword());
+                userService.saveUser(user); 
+                return ResponseEntity.ok(user);
+            })
+            .orElse(ResponseEntity.notFound().build());
+    }
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
+		return userService.findUserById(id)
+				.map(user -> {
+					userService.deleteUser(id);
+					return ResponseEntity.ok().<Void>build();
+				})
+				.orElse(ResponseEntity.notFound().build());
+	}
 
 }
--- alephart/src/main/java/com/stella/alephart/services/UserService.java
+++ alephart/src/main/java/com/stella/alephart/services/UserService.java
@@ -1,6 +1,5 @@
 package com.stella.alephart.services;
 
-// import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,29 +15,23 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
-	// GET all
 	//public List<User> findAllUsers(){
 	//	return userRepository.findAll();
 	//}
 	
-	// GET single
 	public Optional<User> findUserById(Long id){
 		return userRepository.findById(id);
 	}
 		
-	// POST
 	public User saveUser(User user) {
 		return userRepository.save(user);
 	}
 	
-	// PUT #EDIT
 	public User updateUser(Long id, User user) {
 		Optional<User> existingUserOptional = userRepository.findById(id);
 		
-		// Si el usuario existe...
 		if (existingUserOptional.isPresent()) {
 			User existingUser = existingUserOptional.get();
-			
 			// Actualizar datos de usuario === JUST IN CASE ===
 			//existingUser.setFirst_name(user.getFirst_name());
 			//existingUser.setLast_name(user.getLast_name());
@@ -48,17 +41,11 @@ public class UserService {
 			
 			return userRepository.save(user);
 		} else {
-			// Si no se encuentra al usuario
 			throw new RuntimeException("Usuario no encontrado");
 		}
-		
 	}
 	
-	
-	// DELETE
 	public void deleteUser(Long id) {
 		userRepository.deleteById(id);
 	}
-	
-	
 }
