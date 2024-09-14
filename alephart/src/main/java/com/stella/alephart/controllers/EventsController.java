563369cab2acbb38208a43f11bd3bc08a930cd46
ValLpz <val.lpz.glz@gmail.com>
2024-09-14 03:38:54 -0600

    Creación de EventsController, y cambios en models, repository y services de Events

---
+++ alephart/src/main/java/com/stella/alephart/controllers/EventsController.java
@@ -0,0 +1,64 @@
+package com.stella.alephart.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.stella.alephart.models.Events;
+import com.stella.alephart.services.EventsService;
+
+@RestController
+@RequestMapping("/api/Events")
+public class EventsController {
+	
+	@Autowired
+	private EventsService eventsService;
+	
+	@GetMapping
+	public List<Events> getAllEventss() {
+		return eventsService.findAllEvents();
+	}
+	 @GetMapping("/{id}")
+	public ResponseEntity<Events> getEventsById(@PathVariable("id") Long id) {
+		return eventsService.findEventsById(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());}
+	 
+	 @PutMapping("/{id}")
+	 public ResponseEntity<Events> updateEvent(@PathVariable Long id, @RequestBody Events event) {
+	        try {
+	            Events updatedEvent = eventsService.updateEvent(id, event);
+	            return new ResponseEntity<>(updatedEvent, HttpStatus.OK);
+	        } catch (RuntimeException e) {
+	            return ResponseEntity.notFound().build();
+	        }
+	    }
+	 
+	 @PostMapping
+		public Events createEvents(@RequestBody Events events) {
+			return eventsService.saveEvent(events);
+		}
+		
+
+		@DeleteMapping("/{id}")
+			public ResponseEntity<Void> deleteEvents(@PathVariable("id") Long id) {
+				return eventsService.findEventsById(id)
+						.map(events -> {
+							eventsService.deleteEvent(id);
+							return ResponseEntity.ok().<Void>build();
+						})
+						.orElse(ResponseEntity.notFound().build());
+			}
+
+}
--- alephart/src/main/java/com/stella/alephart/services/EventsService.java
+++ alephart/src/main/java/com/stella/alephart/services/EventsService.java
@@ -1,5 +1,48 @@
 package com.stella.alephart.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.stella.alephart.models.Events;
+import com.stella.alephart.repository.EventsRepository;
+
 public class EventsService {
+	
+	@Autowired
+	private EventsRepository eventsRepository;
+	
+	public List<Events> findAllEvents(){
+		return eventsRepository.findAll();
+	}
+		
+	public Optional<Events> findEventsById(Long id) {
+		return eventsRepository.findById(id);
+	}
+	
+	public Events saveEvent(Events event) {
+		return eventsRepository.save(event);
+	}
+	
+	public Events updateEvent(Long id, Events event) {
+		Optional<Events> existingEventsOptional = eventsRepository.findById(id);
+		
+		if (existingEventsOptional.isPresent()) {
+			Events existingEvents = existingEventsOptional.get();
+		
+			 existingEvents.setEvent_description(event.getEvent_description());
+			
+			return eventsRepository.save(existingEvents);
+		} else {
+			
+			throw new RuntimeException("Evento" + id + " no encontrado");
+		}
+	
+	}
+	
+	public void deleteEvent(Long id) {
+		eventsRepository.deleteById(id);
+	}
 
 }
