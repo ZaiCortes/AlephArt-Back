d03a3ce2da1139eac0d249cabdb8df0dde285412
DamZav <damaris.zavala91@gmail.com>
2024-09-13 18:10:32 -0600

    UserProfile stuff + relations between user - userProfile

--- alephart/src/main/java/com/stella/alephart/services/UserProfileService.java
+++ alephart/src/main/java/com/stella/alephart/services/UserProfileService.java
@@ -1,5 +1,52 @@
 package com.stella.alephart.services;
 
-public class UserProfileService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.stella.alephart.models.UserProfile;
+import com.stella.alephart.repository.UserProfileRepository;
+
+public class UserProfileService {
+	// inyección
+	@Autowired
+	private UserProfileRepository userProfileRepository;
+	
+	public List<UserProfile> findAllUsersProfiles(){
+		return userProfileRepository.findAll();
+	}
+	
+	public Optional<UserProfile> findUserById(Long id) {
+		return userProfileRepository.findById(id);
+	}
+	
+	public UserProfile saveUserProfile(UserProfile userProfile) {
+		return userProfileRepository.save(userProfile);
+	}
+	
+	public Optional<UserProfile> updateUserProfile(Long id, UserProfile updatedProfile) {
+        Optional<UserProfile> existingProfile = userProfileRepository.findById(id);
+
+        if (existingProfile.isPresent()) {
+            UserProfile profile = existingProfile.get();
+
+            // Actualizar/modificar
+            profile.setProfile_photo(updatedProfile.getProfile_photo());
+            profile.setBanner(updatedProfile.getBanner());
+            profile.setAbout_me(updatedProfile.getAbout_me());
+            profile.setProfession(updatedProfile.getProfession());
+
+            userProfileRepository.save(profile);
+            return Optional.of(profile); // Devuelve el perfil actualizado
+
+        } else {
+            return Optional.empty(); // Si no se encuentra, devuelve un Optional vacío
+        }
+    }
+	
+	public void deleteUserProfile(Long id) {
+		userProfileRepository.deleteById(id);
+	}
+	
 }
