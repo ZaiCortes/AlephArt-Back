3a319b09e5020662b4d616465c0459ccfed299c2
Diana Ampudia <dianalauraampudia@gmail.com>
2024-09-17 15:34:53 -0600

    cambios user, userProfile y book models

--- alephart/src/main/java/com/stella/alephart/models/Book.java
+++ alephart/src/main/java/com/stella/alephart/models/Book.java
@@ -1,10 +1,15 @@
 package com.stella.alephart.models;
 
+import java.util.Arrays;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 
@@ -27,43 +32,65 @@ public class Book {
 	
 	public Book() {}
 	
-	public Book(Long id_book, byte[] book_photo, String book_name, String book_description) {
+	@OneToOne(mappedBy = "book")
+	@JsonBackReference 
+	private UserProfile userProfile;
+
+	public Book(Long id_book, byte[] book_photo, String book_name, String book_description, UserProfile userProfile) {
 		super();
 		this.id_book = id_book;
 		this.book_photo = book_photo;
 		this.book_name = book_name;
 		this.book_description = book_description;
+		this.userProfile = userProfile;
 	}
-	
-	
+
 	public Long getId_book() {
 		return id_book;
 	}
+
 	public void setId_book(Long id_book) {
 		this.id_book = id_book;
 	}
+
 	public byte[] getBook_photo() {
 		return book_photo;
 	}
+
 	public void setBook_photo(byte[] book_photo) {
 		this.book_photo = book_photo;
 	}
+
 	public String getBook_name() {
 		return book_name;
 	}
+
 	public void setBook_name(String book_name) {
 		this.book_name = book_name;
 	}
+
 	public String getBook_description() {
 		return book_description;
 	}
+
 	public void setBook_description(String book_description) {
 		this.book_description = book_description;
 	}
+
+	public UserProfile getUserProfile() {
+		return userProfile;
+	}
+
+	public void setUserProfile(UserProfile userProfile) {
+		this.userProfile = userProfile;
+	}
+
 	@Override
 	public String toString() {
-		return "Book [id_book=" + id_book + ", book_photo=" + book_photo + ", book_name=" + book_name
-				+ ", book_description=" + book_description + "]";
-	}
+		return "Book [id_book=" + id_book + ", book_photo=" + Arrays.toString(book_photo) + ", book_name=" + book_name
+				+ ", book_description=" + book_description + ", userProfile=" + userProfile + "]";
+	} 
+	
+	
 }
 
--- alephart/src/main/java/com/stella/alephart/models/UserProfile.java
+++ alephart/src/main/java/com/stella/alephart/models/UserProfile.java
@@ -1,6 +1,7 @@
 package com.stella.alephart.models;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.CascadeType;
@@ -11,8 +12,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="userprofile")
 public class UserProfile {
 	
 	@Id
@@ -33,70 +36,109 @@ public class UserProfile {
 	
 	 // USERPROFILE O:O USER -- Relación inversa 
 	@OneToOne(mappedBy = "userProfile") // Con mappedBy -> userP no es la propietaria de la relación
+	@JsonBackReference
     private User user;
 	
 	// USERPROFILE O:O BOOK
 	@OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "book_id_book", referencedColumnName = "id_book")
+	@JsonManagedReference
     private Book book;
 
 	
 	public UserProfile() {}
 
-	public UserProfile(Long id_user_profile, byte profile_photo, byte banner, String about_me, String profession) {
+
+	public UserProfile(Long id_user_profile, byte profile_photo, byte banner, String about_me, String profession,
+			User user, Book book) {
 		super();
 		this.id_user_profile = id_user_profile;
 		this.profile_photo = profile_photo;
 		this.banner = banner;
 		this.about_me = about_me;
 		this.profession = profession;
+		this.user = user;
+		this.book = book;
 	}
+
 
 	public Long getId_user_profile() {
 		return id_user_profile;
 	}
 
+
 	public void setId_user_profile(Long id_user_profile) {
 		this.id_user_profile = id_user_profile;
 	}
+
 
 	public byte getProfile_photo() {
 		return profile_photo;
 	}
 
+
 	public void setProfile_photo(byte profile_photo) {
 		this.profile_photo = profile_photo;
 	}
+
 
 	public byte getBanner() {
 		return banner;
 	}
 
+
 	public void setBanner(byte banner) {
 		this.banner = banner;
 	}
+
 
 	public String getAbout_me() {
 		return about_me;
 	}
 
+
 	public void setAbout_me(String about_me) {
 		this.about_me = about_me;
 	}
+
 
 	public String getProfession() {
 		return profession;
 	}
 
+
 	public void setProfession(String profession) {
 		this.profession = profession;
 	}
 
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	public Book getBook() {
+		return book;
+	}
+
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+
 	@Override
 	public String toString() {
 		return "UserProfile [id_user_profile=" + id_user_profile + ", profile_photo=" + profile_photo + ", banner="
-				+ banner + ", about_me=" + about_me + ", profession=" + profession + "]";
+				+ banner + ", about_me=" + about_me + ", profession=" + profession + ", user=" + user + ", book=" + book
+				+ "]";
 	}
+
 	
 	
 }
